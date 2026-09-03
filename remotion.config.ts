601dda4430be80a97052a0412f0e5e807acb54e6
linearproductionwork <linearproductionwork@gmail.com>
2026-09-03 14:00:16 -0400

    chore: remove project contents

--- remotion.config.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from "@remotion/cli/config";
-
-// Use EGL for WebGL rendering in headless Chromium (required for Mapbox GL)
-Config.setChromiumOpenGlRenderer("egl");
-Config.setVideoImageFormat("jpeg");
-Config.setOverwriteOutput(true);
-Config.setConcurrency(1); // Render frames sequentially to avoid map tile contention
